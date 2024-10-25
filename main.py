3f62824a91965948604df9a3ed2a1ff8d23a44a4
Zony Yu <zony249@gmail.com>
2024-10-25 13:30:03 -0600

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import numpy as np
+
+# image loading 
+def load_image(filepath: str) -> np.array: 
+    pass
+
+
+def view_image(image: np.array) -> None: 
+    pass
+
+
+def create_gaussian_kernel(size:int, stddev:float) -> np.array: 
+    pass
